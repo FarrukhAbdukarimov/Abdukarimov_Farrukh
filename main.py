00d916e6a218aad9c908450487aa8361081569a7
Farrukh Abdukarimov <faru171088@gmail.com>
2023-10-10 16:44:58 +0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# лорпавы
+# Data Turlari (2-3 hafta):
+# ● Python-da har xil ma'lumot turlari (dict, tuple, list, json, xml, float, int, str)ni o'rganing va ulardan qanday foydalanishni tushunish.
+# Python Asosiy Tushunchalari (2-3 hafta):
+# ● Print: Ma'lumotlarni ekranga chiqarishni o'rganing.
+# ● For loop: Sikl orqali ma'lumotlar to'plamlarini o'rab chiqishni o'rganing.
+# ● While: Sikl amal qilish vaqtincha davom ettirishni o'rganing.
+# ● Try, Except, Finally: Xato bilan dastur bajarish va uning muomalasini bartaraf qilishni o'rganing.
+# ● If, Elif, Else: Shartlar bo'yicha kodni bajarishni o'rganing.
+# OOP Tamoyillari (2-3 hafta):
+# ● Def: Funksiyalarni yaratish va ulardan foydalanishni o'rganing.
+# ● Class: Obyektlar va klasslarni yaratish va ulardan foydalanishni o'rganing.
+# PostgreSQL Database (3 hafta):
+# ● Database yaratish va ulanishni o'rganing.
+# ● CRUD (Create, Read, Update, Delete) operatsiyalarini bazaga qo'llab-quvvatlashni o'rganing.
+# ● Bazaga buyruqlarni bajarish (SQL buyruqlarini yozish va ijro etish)ni o'rganing.
+# FastAPI vs SQLAlchemy (4-5 hafta):
+# ● FastAPI: API yaratish uchun FastAPI frameworkini o'rganing.
+# ● SQLAlchemy: Bazaga ulanish uchun SQLAlchemy toolkitini o'rganing.
+# ● Pagination: San'atlar sahifalarni qo'llab-quvvatlashni o'rganing.
+# ● URLs: FastAPI'da URL-routingni o'rganing.
+# ● Authentication: Foydalanuvchini tasdiqlashni o'rganing.
+# ● Token: Autentifikatsiya tokenlarini o'rganing.
+# ● Schema: Ma'lumotlar modellarini yaratish va ulardan foydalanishni o'rganing.
+# Manba: Udemy online course
+#  
